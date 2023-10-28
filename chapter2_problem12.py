e814419e9e6be4a57b6c91bd922db4d2a80d3fd0
feysalgh <amu.feysal@gmail.com>
2023-10-28 20:37:51 +0330

    v3

--- chapter2_problem12.py
+++ chapter2_problem12.py
@@ -1,4 +1,14 @@
 import equations
+import matplotlib.pyplot as plt
+
 gamma_g = 0.65 # specific gravity of gas
 T = 110 # temperature in C
+T = equations.C_to_F(T)
+Ppc = equations.calculate_Ppc_ahmed_method(gamma_g, 0, 0, 0)
+Ppr = [P / Ppc for P in range(14, 8000)]
 
+plt.plot(range(14, 8000), Ppr)
+plt.xlabel('Pressure (psi)')
+plt.ylabel('Ppr')
+plt.title('Pseudo-Reduced Pressure vs Pressure')
+plt.show()
